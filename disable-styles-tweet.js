22e9e64f4745f328c8c5db51d38d83b8456ca0c7
Petr Huřťák <petr.hurtak@gmail.com>
2015-07-16 08:48:39 +0200

    save 1 character

--- disable-styles-tweet.js
+++ disable-styles-tweet.js
@@ -1,5 +1,5 @@
-// 138 characters, 3 global variables, ES5
-javascript:x=document.querySelectorAll('link,style,[style]'),i=x.length;while(i--)a=x[i],a.style.cssText?a.style.cssText='':a.outerHTML=''
+// 137 characters, 3 global variables, ES5
+javascript:x=document.querySelectorAll('link,style,[style]');for(i=x.length;i--;)a=x[i],a.style.cssText?a.style.cssText='':a.outerHTML=''
 
 // 134 characters, 0 global variables, ES6
 javascript:Array.from(document.querySelectorAll('link,style,[style]')).forEach(e=>{e.style.cssText?e.style.cssText='':e.outerHTML=''})
