23ef9faa9fdb9b0f08c9681d7536ccf583dd52db
Petr Huřťák <petr.hurtak@gmail.com>
2015-07-16 08:44:23 +0200

    add ES6 version

--- disable-styles-tweet.js
+++ disable-styles-tweet.js
@@ -1,2 +1,5 @@
-x = document.querySelectorAll('link, style, [style]'), i = x.length;
-while (i--) a=x[i], a.style[b = 'cssText'] ? a.style[b] = '' : a.outerHTML = ''
+// 138 characters, 3 global variables, ES5
+javascript:x=document.querySelectorAll('link,style,[style]'),i=x.length;while(i--)a=x[i],a.style.cssText?a.style.cssText='':a.outerHTML=''
+
+// 134 characters, 0 global variables, ES6
+javascript:Array.from(document.querySelectorAll('link,style,[style]')).forEach(e=>{e.style.cssText?e.style.cssText='':e.outerHTML=''})
