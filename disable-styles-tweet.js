f5af354948ae6348c8ad8c72544f4b7ea40dec93
Petr Huřťák <petr.hurtak@gmail.com>
2015-07-17 20:51:24 +0200

    Delete disable-styles-tweet.js

--- disable-styles-tweet.js
+++
@@ -1,5 +0,0 @@
-// 137 characters, 3 global variables, ES5
-javascript:x=document.querySelectorAll('link,style,[style]');for(i=x.length;i--;)a=x[i],a.style.cssText?a.style.cssText='':a.outerHTML=''
-
-// 134 characters, 0 global variables, ES6
-javascript:Array.from(document.querySelectorAll('link,style,[style]')).forEach(e=>{e.style.cssText?e.style.cssText='':e.outerHTML=''})
